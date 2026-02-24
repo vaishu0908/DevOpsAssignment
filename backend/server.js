54a623fc0f898101ab32029a7790318c4ee8643d
vaishu0908 <vaishnavipachpande0908@gmail.com>
2026-02-24 19:21:09 +0530

    Assignment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 // parse requests of content-type - application/json
 app.use(express.json());
 
+app.use(cors({
+  origin: "*"
+}));
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
